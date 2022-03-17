56297a888b3688e57d904d1613dea074b577bd33
Josh Sparkes <jshsparx@gmail.com>
2022-03-17 16:09:21 -0600

    added test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,27 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const breed = args[0];
 
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   //console.log('error:', error); // Print the error if one occurred
   //console.log(typeof body);
-  if (error) {
-    console.log("error: ", error)
-    return
-  };
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-  const data = JSON.parse(body);
-  if (!data.length) {
-    console.log("Breed not found")
-    return
-  };
-  console.log(data[0].description);
+    if (error) {
+      console.log("error: ", error);
+      return;
+    }
+    //console.log('body:', body); // Print the HTML for the Google homepage.
+    const apiResponse = JSON.parse(body);
+    if (!apiResponse.length) {
+    //console.log("Breed not found")
+      callback("Breed not found", null);
+      return;
+    }
+    callback(null, apiResponse[0].description);
+  //console.log(data[0].description);
   //console.log(typeof data);
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
 
